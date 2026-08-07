64fb18108fa5fba0e9da6f74dc28ee124282162a
Kaidstor <kaidstor@gmail.com>
2026-08-07 22:34:30 +0500

    sec-app 0.2.1

--- Casks/sec-app.rb
+++ Casks/sec-app.rb
@@ -1,6 +1,6 @@
 cask "sec-app" do
-  version "0.2.0"
-  sha256 "46324707c44ff3a6ef77092ee5e1a284653997194c97ffd869087e97020cdabe"
+  version "0.2.1"
+  sha256 "f8436ebc526c2cdd7c6707bc68c9e00c0db98aa60dd055c1595e3c48752bfcad"
 
   url "https://github.com/Kaidstor/sec/releases/download/app-v#{version}/sec-app_#{version}_darwin-aarch64.dmg"
   name "sec"
